99a2fe6cbfc3ca20e5b5807d76ab1d5b2944a6f6
Ajay Sharma <gridking111@gmail.com>
2026-08-21 15:02:13 +0300

    Fix import extension in profile page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import ProfilePageContent from "./ProfilePageContent.tsx";
+import ProfilePageContent from "./ProfilePageContent";
 
 export default function ProfilePage() {
   return (
