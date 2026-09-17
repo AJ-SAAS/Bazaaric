84627b609931923f2cd5777c07b8ed803cbc757f
Ajay Sharma <gridking111@gmail.com>
2026-09-17 11:51:58 +0300

    Add /auth to /register redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/auth",
+        destination: "/register",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
